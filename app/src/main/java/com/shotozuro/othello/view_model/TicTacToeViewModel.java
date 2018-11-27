5f6d522aa774a9b3256c8ed635cd5a0cb1cacaa3
shotozuro <shotozuro@gmail.com>
2018-11-27 11:37:40 +0700

    implement MVVM architecture

--- app/src/main/java/com/shotozuro/othello/view_model/TicTacToeViewModel.java
+++ app/src/main/java/com/shotozuro/othello/view_model/TicTacToeViewModel.java
@@ -1,11 +1,11 @@
-package com.shotozuro.othello.presenter;
+package com.shotozuro.othello.view_model;
 
 import com.shotozuro.othello.model.Board;
 import com.shotozuro.othello.model.Cell;
 import com.shotozuro.othello.model.Player;
 import com.shotozuro.othello.view.MainActivity;
 
-public class OthelloPresenter {
+public class TicTacToeViewModel {
     public MainActivity activity;
     public Board board;
     public Cell cell;
